469eb2b704705566b91ea3804d06ecca36a8bcc5
jamariod <j25ghs99@aol.com>
2020-12-14 14:59:54 -0500

    Initial Commit

---
+++ CollectionViewJSON/CustomCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CustomCollectionViewCell.swift
+//  CollectionViewJSON
+//
+//  Created by Jamario Davis on 12/12/20.
+//
+
+import UIKit
+
+class CustomCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+}
